4c8ab61c65a2cd01352e570fe1d7b54a77e30e41
Patrick Kelly <patdkelly5@gmail.com>
2024-07-15 21:41:42 +1000

    THIS is where the use-client horseshit starts

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,4 @@
-import { formatAmount } from "@/lib/utils";
-import React from "react";
+import AnimatedCounter from "./AnimatedCounter";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -16,7 +15,7 @@ const TotalBalanceBox = ({
           <p className="total-balance-label">Total Current Balance</p>
 
           <p className="total-balance-amount flex-center gap-2">
-            {formatAmount(totalCurrentBalance)}
+            <AnimatedCounter amount={totalCurrentBalance} />
           </p>
         </div>
       </div>
