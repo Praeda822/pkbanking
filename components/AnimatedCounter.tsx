517816dacc16ba3b5cfb50977871293f32a09e5b
Patrick Kelly <patdkelly5@gmail.com>
2024-07-15 21:43:00 +1000

    now I import CountUp and the whole fucking thing breaks

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -1,6 +1,8 @@
+"use-client";
+
 import React from "react";
 
-const AnimatedCounter = () => {
+const AnimatedCounter = ({ amount }: { amount: number }) => {
   return <div>AnimatedCounter</div>;
 };
 
